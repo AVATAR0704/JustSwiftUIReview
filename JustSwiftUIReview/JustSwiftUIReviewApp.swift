569735a4c30e1113281a911bb25f02b7f35d1945
Samandar <samandartoshpulatov0704@gmail.com>
2024-05-01 10:00:51 +0500

    Initial Commit

---
+++ JustSwiftUIReview/JustSwiftUIReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustSwiftUIReviewApp.swift
+//  JustSwiftUIReview
+//
+//  Created by Samandar on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JustSwiftUIReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
