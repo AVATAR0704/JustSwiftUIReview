2374ceb755bc4b883ecccddcc54a84873fcb5afd
Samandar <samandartoshpulatov0704@gmail.com>
2024-05-08 17:55:17 +0500

    Another one

--- JustSwiftUIReview/Basics/TextFieldB.swift
+++ JustSwiftUIReview/Basics/TextFieldB.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct TextFieldB: View {
+    
+    @State var titleofTF: String = ""
+    @State var data: [String] = []
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        NavigationView {
+            
+            ScrollView {
+                VStack {
+                   
+                    TextField("Enter Text", text: $titleofTF)
+                        .padding(10)
+                        .background(Color.gray.opacity(0.3))
+                        .cornerRadius(10)
+                    
+                    Button {
+                        
+                        if isTextApropriate() {
+                            addNewElemant()
+                        }
+                        
+                        
+                    } label: {
+                        Text("Save")
+                            .foregroundColor(.white)
+                            .font(.system(size: 20))
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(isTextApropriate() ? Color.blue : Color.gray)
+                            .cornerRadius(10)
+                    }
+                    .disabled(!isTextApropriate())
+                    
+                    
+                    ForEach(data, id: \.self) { data in
+                        Text(data)
+                            //.font(.largeTitle)
+                    }
+
+                    Spacer()
+                }
+                .padding()
+                
+            }
+            .navigationTitle("All Information")
+        }
     }
+    
+    
+    func isTextApropriate() -> Bool{
+        
+        return titleofTF.count > 2 ? true : false
+    }
+    
+    func addNewElemant() {
+        
+        data.append(titleofTF)
+        
+        titleofTF = ""
+        
+    }
+    
 }
 
 struct TextFieldB_Previews: PreviewProvider {
